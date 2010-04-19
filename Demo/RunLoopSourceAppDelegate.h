fb4f4a202332584f3403257f166cb811765906a1
Dave Dribin <dave@dribin.org>
2010-04-18 22:14:02 -0500

    Remove DDRunLoopPoker

--- Demo/RunLoopSourceAppDelegate.h
+++ Demo/RunLoopSourceAppDelegate.h
@@ -1,13 +1,6 @@
 //
-//  RunLoopSourceAppDelegate.h
-//  RunLoopSource
-//
-//  Created by Dave Dribin on 4/17/10.
-//  Copyright 2010 Bit Maki, Inc.. All rights reserved.
-//
 
 #import <Cocoa/Cocoa.h>
-#import "DDRunLoopPoker.h"
 #import "DDAudioBufferQueue.h"
 
 @class DDAudioBufferQueue;
@@ -15,7 +8,6 @@
 
 @interface RunLoopSourceAppDelegate : NSObject <NSApplicationDelegate, DDAudioQueueDelegate> {
     NSWindow *window;
-    DDRunLoopPoker * _poker;
     DDAudioBufferQueue * _audioQueue;
     DDAudioBuffer * _activeBuffer;
     int _counter;
--- Demo/RunLoopSourceAppDelegate.m
+++ Demo/RunLoopSourceAppDelegate.m
@@ -1,10 +1,4 @@
 //
-//  RunLoopSourceAppDelegate.m
-//  RunLoopSource
-//
-//  Created by Dave Dribin on 4/17/10.
-//  Copyright 2010 Bit Maki, Inc.. All rights reserved.
-//
 
 #import "RunLoopSourceAppDelegate.h"
 #import "DDAudioBufferQueue.h"
